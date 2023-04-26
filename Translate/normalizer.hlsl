3d432a852934e6214cebca01c6154a1a36b393e0
H4L609 <aaaa@aaaa.com>
2023-04-26 21:44:19 +0900

    正規化関数の説明を書き換えた。

--- Translate/normalizer.hlsl
+++ Translate/normalizer.hlsl
@@ -1,18 +1,29 @@
 #ifndef NORMALIZER_INCLUDED
 #define NORMALIZER_INCLUDED
 
+// -1~1の値を、0~1に変換する
+// dot, cos, sinなどに使う
 float sin2uv(float i){
     return (i+1)*0.5;
-    // -1~1の値を、0~1に変換するだけ
 };
 
+
+// 0~1の値を、-1~1に変換する
 float uv2sin(float i){
     return (i*2)-1;
-    // 0~1の値を、-1~1に変換するだけ
 };
 
-/* スカラーの正規化をする関数
-    sin のように、入力値が定まっている関数の iMin 下限値と iMax 上限値 を渡し、
+/* 
+  スカラーの正規化をする関数
+    入力値 target と、
+    入力値が取りうる 下限値 iMin と iMax 上限値 を渡し、
+    出力値に渡したい 下限値 oMin と oMax 上限値 を返す。
+
+  線形補間の基本
+    元となる線分の最小値 min と最大値 max
+    min < 点P < max である、点Pが、minとmaxの間のどの位置にいるのか、割合で示したい。
+    そういうときは、(P - min) / (max - min)で求められる。
+
 */
 float mlerp
 (
@@ -23,12 +34,6 @@ float mlerp
     float target
 )
 {
-    /* 線形補間の基本
-        元となる線分の最小値 min と最大値 max
-        min < 点P < max である、点Pが、minとmaxの間のどの位置にいるのか、割合で示したい。
-        そういうときは、(P - min) / (max - min)で求められる。
-
-    */
     float w = (target - iMin) / (iMax-iMin);
     return lerp(oMin, oMax, w);
 
